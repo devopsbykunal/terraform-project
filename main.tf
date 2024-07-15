b0176119937fe1d498a1d7608ba9c23d5a24fb43
devopsbykunal <globalinformationtracker2002@gmail.com>
2024-07-15 14:18:11 +0800

    Update main.tf
    Signed-off-by: devopsbykunal <globalinformationtracker2002@gmail.com>

--- main.tf
+++ main.tf
@@ -114,8 +114,8 @@ resource "aws_route_table_association" "b" {
 
 # EC2 Instances
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"  # Replace with your AMI ID
-  instance_type          = "t2.micro"
+  ami                    = "ami-0249211c9916306f8"  # Replace with your AMI ID
+  instance_type          = "t3.micro"
   availability_zone      = "us-east-1a"
   key_name               = "apr6pm"  # Replace with your key pair name
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
@@ -128,8 +128,8 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"  # Replace with your AMI ID
-  instance_type          = "t2.micro"
+  ami                    = "ami-0249211c9916306f8"  # Replace with your AMI ID
+  instance_type          = "t3.micro"
   availability_zone      = "us-east-1b"
   key_name               = "apr6pm"  # Replace with your key pair name
   vpc_security_group_ids = [aws_security_group.webserver-sg.id]
@@ -142,8 +142,8 @@ resource "aws_instance" "webserver2" {
 }
 
 resource "aws_instance" "appserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"  # Replace with your AMI ID
-  instance_type          = "t2.micro"
+  ami                    = "ami-0249211c9916306f8"  # Replace with your AMI ID
+  instance_type          = "t3.micro"
   availability_zone      = "us-east-1a"
   key_name               = "apr6pm"  # Replace with your key pair name
   vpc_security_group_ids = [aws_security_group.appserver-sg.id]
@@ -155,8 +155,8 @@ resource "aws_instance" "appserver1" {
 }
 
 resource "aws_instance" "appserver2" {
-  ami                    = "ami-0d5eff06f840b45e9"  # Replace with your AMI ID
-  instance_type          = "t2.micro"
+  ami                    = "ami-0249211c9916306f8"  # Replace with your AMI ID
+  instance_type          = "t3.micro"
   availability_zone      = "us-east-1b"
   key_name               = "apr6pm"  # Replace with your key pair name
   vpc_security_group_ids = [aws_security_group.appserver-sg.id]
@@ -346,18 +346,6 @@ resource "aws_s3_bucket" "example" {
     Name        = "rahamtestbycketterra7788abcdefxxc"
     Environment = "Dev"
   }
-}
-
-# IAM Users
-resource "aws_iam_user" "one" {
-  for_each = toset(var.iam_users)
-  name     = each.key
-}
-
-variable "iam_users" {
-  description = "Set of IAM user names"
-  type        = set(string)
-  default     = ["user1", "user2", "user3", "user4"]
 }
 
 # IAM Group
